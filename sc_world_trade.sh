e0ce255d73b33a38c08620773a5884b4674a3542
Sam Wise <samthemanwise@gmail.com>
2024-09-03 17:10:37 -0600

    attempts at final results

--- sc_world_trade.sh
+++ sc_world_trade.sh
@@ -2,11 +2,11 @@
 #***** NOTE: run this using: sg grp_supplychainai "sbatch thefilename"
 
 #SBATCH --time=72:00:00   # walltime
-#SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gpus=1
 #SBATCH -C 'pascal'   # features syntax (use quotes): -C 'a&b&c&d'
-#SBATCH --mem-per-cpu=32G  # memory per CPU core
+#SBATCH --mem-per-cpu=64G  # memory per CPU core
 #SBATCH -J "seal_test"   # job name
 
 if [ "$(id -gn)" != "grp_supplychainai" ]; then
